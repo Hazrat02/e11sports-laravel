f29dea0c0fbc5f8176f890e6bb2e726372f1830a
Hazrat02 <hazratbd80@gmail.com>
2023-12-24 10:20:15 -0800

    home deshboard3

--- core/resources/views/admin/game/cricket.blade.php
+++ core/resources/views/admin/game/cricket.blade.php
@@ -65,12 +65,13 @@
                                                         style="color: rgb(226, 218, 206)">
                                                         {{ $match->start }}</Span></p>
                                             </div>
-                                            <a class="widget-two__btn" href="{{ route('admin.game.cricketinf', $match->id ) }}">@lang('Place in bet')</a>
+                                            <a class="widget-two__btn"
+                                                href="{{ route('admin.game.cricketinf', $match->id) }}">@lang('Place in bet')</a>
                                         </div>
 
 
                                     </div>
-                             
+
                                 </div>
                                 {{-- <div>
                                     <h2>{{ $match['name'] }}</h2>
@@ -90,122 +91,102 @@
             </div>
         </div>
     </div>
-    
-   
 
-   
-        <div class="modal fade " id="cronModal" role="dialog" tabindex="-1"  aria-hidden="true">
-            <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h5 class="modal-title" id="exampleModalLongTitle">@lang('Create Cricket Games')</h5>
-                        <span class="btn-close" data-bs-dismiss="modal" type="button" aria-label="Close"></span>
-                    </div>
-                    <div class="modal-body">
-                        <form action="" method="POST">
-                            @csrf
-                            <div class="card-body">
-                                <div class="row">
-                                    <div class="col-md-4">
-                                        <div class="form-group">
-                                            <div class="image-upload">
-                                                <div class="thumb">
-                                                    <div class="avatar-preview">
-                                                     
-                                                        <div class="avatar-edit">
-                                                            <input class="profilePicUpload" id="profilePicUpload" name="image" type="file" accept=".png, .jpg, .jpeg" requierd>
-                                                            <label class="bg--primary" for="profilePicUpload">@lang('Post image')</label>
-                                                            <small class="text-facebook mt-2">@lang('Supported files:') <b>@lang('jpeg, jpg')</b>. @lang('Image will be resized into') </small>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
+
+
+
+    <div class="modal fade " id="cronModal" role="dialog" tabindex="-1" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLongTitle">@lang('Create Cricket Games')</h5>
+                    <span class="btn-close" data-bs-dismiss="modal" type="button" aria-label="Close"></span>
+                </div>
+                <div class="modal-body">
+                    <form action="" method="POST">
+                        @csrf
+                        <div class="card-body">
+                            <div class="row">
+
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+                                        <label>@lang('Team A Name')</label>
+                                        <input class="form-control" name="t1" type="text" value=""
+                                            placeholder="@lang('Bangladesh')" required>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+                                        <label>@lang('Team B Name')</label>
+                                        <input class="form-control" name="t2" type="text" value=""
+                                            placeholder="@lang('India')" required>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+                                        <label>@lang('Team B Name')</label>
+                                        <input class="form-control" name="t2" type="text" value=""
+                                            placeholder="@lang('India')" required>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+
+                                        <label>@lang('Minimum Invest Amount')</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" name="min" type="number" value=""
+                                                step="any" min="1" placeholder="@lang('Minimum Invest Amount')" required>
+                                            <span class="input-group-text" id="basic-addon2">{{ $general->cur_sym }}</span>
+                                        </div>
+                                        <label>@lang('Minimum Invest Amount')</label>
+                                        <input class="form-control" name="min" type="number" value=""
+                                            placeholder="@lang('Minimum Invest Amount')" required>
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+                                        <label>@lang('Maximum Invest Amount')</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" name="max" type="number" value=""
+                                                step="any" min="1" placeholder="@lang('Maximum Invest Amount')" required>
+                                            <span class="input-group-text"
+                                                id="basic-addon2">{{ $general->cur_sym }}</span>
+                                        </div>
+
+                                    </div>
+
+                                </div>
+                                <div class="col-md-6 col-12">
+                                    <div class="form-group">
+                                        <label>@lang('Commission fee')</label>
+                                        <div class="input-group mb-3">
+                                            <input class="form-control" name="fee" type="number" value=""
+                                                placeholder="@lang('Commission fee')">
+                                            <span class="input-group-text" id="basic-addon2">@lang('%')</span>
                                         </div>
                                     </div>
-                                    <div class="col-md-8">
-                                        <div class="form-group">
-                                            <label>@lang('Game Name')</label>
-                                            <input class="form-control" name="name" type="text" value="" placeholder="@lang('Game Name')" required>
-                                        </div>
-                                        <div class="row">
-                                            <div class="col-md-5">
-                                                <div class="card border--primary">
-                                                    <h5 class="card-header bg--primary">@lang('Play Amount')</h5>
-                                                    <div class="card-body">
-                                                        <div class="form-group">
-                                                            <label>@lang('Minimum Invest Amount')</label>
-                                                            <div class="input-group mb-3">
-                                                                <input class="form-control" name="min" type="number" value="" step="any" min="1" placeholder="@lang('Minimum Invest Amount')" required>
-                                                                <span class="input-group-text" id="basic-addon2">{{ $general->cur_sym }}</span>
-                                                            </div>
-                                                        </div>
-                                                        <div class="form-group">
-                                                            <label>@lang('Maximum Invest Amount')</label>
-                                                            <div class="input-group mb-3">
-                                                                <input class="form-control" name="max" type="number" value="" step="any" min="1" placeholder="@lang('Maximum Invest Amount')" required>
-                                                                <span class="input-group-text" id="basic-addon2">{{ $general->cur_sym }}</span>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                            <div class="col-md-7">
-                                                <div class="card border--primary">
-                                                    <h5 class="card-header bg--primary">@lang('Win Setting') </h5>
-                                                    <div class="card-body">
-                                                        <div class="row">
-                                                            <div class="col-md-12">
-                                                                <div class="form-group">
-                                                                    <label>@lang('Winning Chance')</label>
-                                                                    <div class="input-group mb-3">
-                                                                        <input class="form-control" name="probable" type="number" value="" placeholder="@lang('Winning Chance')">
-                                                                        <span class="input-group-text" id="basic-addon2">@lang('%')</span>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <div class="form-group">
-                                                                    <label>@lang('Win Amount')</label>
-                                                                    <div class="input-group mb-3">
-                                                                        <input class="form-control" name="win" type="number" value="" step="any" placeholder="@lang('Win')">
-                                                                        <span class="input-group-text" id="basic-addon2">@lang('%')</span>
-                                                                    </div>
-                                                                </div>
-                                                            </div>
-                                                            <div class="col-md-6">
-                                                                <div class="form-group">
-                                                                    <label>@lang('Invest')</label>
-                                                                    <input name="invest_back" data-width="100%" data-onstyle="-success" data-offstyle="-danger" data-bs-toggle="toggle" data-on="@lang('Give Back')" data-off="@lang('No Back"')" type="checkbox">
-                                                                </div>
-                                                            </div>
-                                                        </div>
-                                                    </div>
-                                                </div>
-                                            </div>
-                                        </div>
-                                    </div>
+
                                 </div>
-                                <div class="col-lg-12">
-                                    <div class="card border--primary mt-3">
-                                        <h5 class="card-header bg--primary">@lang(' Game Instruction')</h5>
-                                        <div class="card-body">
-                                            <div class="form-group">
-                                                <textarea class="form-control border-radius-5 nicEdit" name="instruction" rows="8">fggfgfgfgf</textarea>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <div class="mt-3">
-                                    <button class="btn btn--primary w-100 h-45" type="submit">@lang('Submit')</button>
-                                </div>
+
+
                             </div>
-                        </form>
-                    </div>
+
+                            <div class="mt-3">
+                                <button class="btn btn--primary w-100 h-45" type="submit">@lang('Submit')</button>
+                            </div>
+                        </div>
+                    </form>
                 </div>
             </div>
         </div>
+    </div>
 @endsection
 
 @push('breadcrumb-plugins')
-<button  class="btn btn-outline--info" data-bs-toggle="modal" type="button"  data-bs-target="#cronModal">+ Add Game</button>
+    <button class="btn btn-outline--info" data-bs-toggle="modal" type="button" data-bs-target="#cronModal">+ Add
+        Game</button>
 @endpush
