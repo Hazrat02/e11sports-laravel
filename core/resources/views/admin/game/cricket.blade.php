52a3d4f075f7af5f323fdbdeff0080f3ca3365e0
Hazrat02 <hazratbd80@gmail.com>
2023-12-24 12:03:07 -0800

    home deshboard3

--- core/resources/views/admin/game/cricket.blade.php
+++ core/resources/views/admin/game/cricket.blade.php
@@ -33,21 +33,7 @@
                             @else
                                 <p>No Betting Games !</p>
                             @endif
-                                <div class="widget-two style--two box--shadow2 b-radius--5 bg--19">
-                                    <div class="widget-two__icon b-radius--5 bg--primary">
-                                        <i class="las la-money-bill-wave-alt"></i>
-                                    </div>
-                                    <div class="widget-two__content">
-                                        <h3 class="text-white">Bangladesh</h3>
-                                        <p class="text-white">@lang('VS')</p>
-                                        <h3 class="text-white">India</h3>
-                                    </div>
-                                    <div>
-                                        <a class="widget-two__btn" href=""> Details</a>
-
-                                    </div>
-
-                                </div>
+                              
 
                             </div>
                         </div>
