dbfe6e2324e38aa9ac408826386d66fcecefe601
Hazrat02 <hazratbd80@gmail.com>
2023-12-31 08:47:32 -0800

    admin dashboard game

--- core/resources/views/admin/withdraw/detail.blade.php
+++ core/resources/views/admin/withdraw/detail.blade.php
@@ -65,8 +65,7 @@
                     </li>
                     @if($withdrawal->withdraw_information)
                     <li class="list-group-item d-flex justify-content-between align-items-center">
-                        @lang('{{$withdrawal->withdraw_information->name}}')
-                        <p>{{$withdrawal->withdraw_information->value}}</p>
+                       dsdsdsd
                     </li>
                     @endif
                 </ul>
