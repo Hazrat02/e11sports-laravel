427bfb447b7942ac07ed3f58bbec51b28dd2d5b1
Hazrat02 <hazratbd80@gmail.com>
2024-01-10 09:59:30 -0800

    seo manage

--- core/resources/views/admin/game/game_edit.blade.php
+++ core/resources/views/admin/game/game_edit.blade.php
@@ -75,12 +75,9 @@
                                                             </div>
                                                         </div>
                                                     </div>
-                                                    {{-- <div class="col-md-6">
-                                                        <div class="form-group">
-                                                            <label>@lang('Invest')</label>
-                                                            <input name="invest_back" data-width="100%" data-onstyle="-success" data-offstyle="-danger" data-bs-toggle="toggle" data-on="@lang('Give Back')" data-off="@lang('No Back"')" type="checkbox" @checked($game->invest_back)>
-                                                        </div>
-                                                    </div> --}}
+                                                   
+                                                            <input name="invest_back" type="hidden" value="{{$game->invest_back}}">
+                                                     
                                                 </div>
                                             </div>
                                         </div>
