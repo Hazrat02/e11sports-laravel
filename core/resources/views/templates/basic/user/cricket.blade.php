6a58cdfed159ef8197ca49abe0b9dbf5ca94bae1
Hazrat02 <hazratbd80@gmail.com>
2024-01-14 08:34:24 -0800

    user dashboard

--- core/resources/views/templates/basic/user/cricket.blade.php
+++ core/resources/views/templates/basic/user/cricket.blade.php
@@ -33,12 +33,12 @@
                                 </div>
                                 <div class="panel marcadores">
                                     <div class="equipo-marcador  izquierdo">
-                                        <p style="font-size: 10px !important" class="com">{{$match->t1}}</p>
+                                        <p style="font-size: 20px !important" >{{$match->t1}}</p>
 
                                     </div>
                                     <div class="marcador-tiempo"><time>VS</time></div>
                                     <div class="equipo-marcador  derecho">
-                                        <p style="font-size: 10px !important" class="com">{{$match->t2}}</p>
+                                        <p style="font-size: 20px !important">{{$match->t2}}</p>
                                     </div>
                                 </div>
                                 <div class="d-flex justify-content-center">
