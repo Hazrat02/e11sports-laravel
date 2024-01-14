dc616ea54d6747e7fbb2615df389e56d8f3f88f2
Hazrat02 <hazratbd80@gmail.com>
2024-01-14 08:17:43 -0800

    user dashboard

--- core/resources/views/templates/basic/sections/live_game.blade.php
+++ core/resources/views/templates/basic/sections/live_game.blade.php
@@ -16,9 +16,51 @@ $testimonials = getContent('testimonial.element');
                 <div class="testimonial-slider">
                     @foreach ($bets as $bet)
                     <div class="single-slide">
-                        sadasdadasdasda
-                        {{-- <div class="testimonial-card">
-                            <div class="testimonial-card__content">
+                        
+                        <div class="testimonial-card">
+                            <div class="contenedorEquipo">
+                                <div class="base">
+    
+                                    <div class="panel panel-superior">
+                                        <div class="equipo-superior izquierdo"><img alt="nombreEquipo" class="float-izquierda"
+                                                src="{{$bet->t1_img}}">
+    
+                                            <div class="" style="position: absolute;top:0%;left:31%">
+                                                <p class="com"><span >Commition : </span> <span style="color: #e176ce"> {{$bet->fee}}%</span>
+                                                    @if ($bet->isbet == '1')
+                                                    <p class="com"><span>Bet : </span> <span style="color: #32874a"> Runing</span>
+                                                    @else
+                                                    <p class="com"><span>Bet : </span> <span style="color: #be0439"> Stop</span>
+                                                    @endif 
+                                                </div>
+    
+                                        </div>
+                                        <div class="equipo-superior derecho"><img alt="nombreEquipo" class="float-derecha"
+                                                src='{{$bet->t2_img}}'>
+    
+    
+                                        </div>
+                                    </div>
+                                    <div class="panel marcadores">
+                                        <div class="equipo-marcador  izquierdo">
+                                            <p class="float-izquierda">{{$bet->t1}}</p>
+    
+                                        </div>
+                                        <div class="marcador-tiempo"><time>VS</time></div>
+                                        <div class="equipo-marcador  derecho">
+                                            <p class="">{{$bet->t2}}</p>
+                                        </div>
+                                    </div>
+                                    <div class="d-flex justify-content-center">
+    
+                                        <div class="inferior-boton  float-izquierda"><a
+                                                href="{{ route('user.play.cricketbet',$bet->id) }}"><button>Bet Now</button></a>
+                                        </div>
+    
+                                    </div>
+                                </div>
+                            </div>
+                            {{-- <div class="testimonial-card__content">
                                 <div class="testimonial-card__content-inner">
                                     <p>{{ __(@$testimonial->data_values->quote) }}</p>
                                 </div>
@@ -26,8 +68,8 @@ $testimonials = getContent('testimonial.element');
                             <div class="testimonial-card__thumb">
                                 <img src="{{ getImage('assets/images/frontend/testimonial/' . @$testimonial->data_values->image, '100x100') }}" alt="image">
                             </div>
-                            <h6 class="name mt-2">{{ __(@$testimonial->data_values->name) }}</h6>
-                        </div> --}}
+                            <h6 class="name mt-2">{{ __(@$testimonial->data_values->name) }}</h6> --}}
+                        </div>
                     </div>
                     @endforeach
                 </div>
@@ -35,3 +77,352 @@ $testimonials = getContent('testimonial.element');
         </div>
     </div>
 </section>
+@push('style')
+    <style type="text/css">
+        .contenedorEquipo {
+            margin: 10px 0px 15px 0px;
+        }
+
+        .base {
+
+            width: 90%;
+            margin: 0px auto;
+            box-shadow: 1px 1px 1px 1px black;
+            border-radius: 10px;
+            background: rgba(0, 0, 0, 1);
+            overflow: hidden;
+            border: 2px solid white;
+            font-family: bebas, Verdana, Geneva, sans-serif;
+            color: white;
+            padding-bottom: 10px;
+        }
+
+        .panel {
+            width: 100%;
+            box-shadow: 0px 0px 0px 1px rgba(255, 255, 255, 1);
+            background-color: rgba(0, 0, 0, 1);
+            /* display: block; */
+            /* float: left; */
+            position: relative;
+            top: 0px;
+            height: 75px;
+        }
+
+        .equipo-superior {
+            width: 50%;
+            float: left;
+            color: white;
+            height: 100%;
+            overflow: hidden;
+        }
+
+        .equipo-superior img {
+
+            width: 100px;
+            margin: 0px 20px;
+        }
+
+        .float-izquierda {
+            float: left !important;
+        }
+
+        .float-derecha {
+            float: right !important;
+        }
+
+        .panel-superior .izquierdo {
+            background: linear-gradient(to right, rgba(255, 175, 75, 1) 0%, rgba(255, 146, 10, 0) 100%);
+        }
+
+        .panel-superior .izquierdo span {
+            float: left;
+        }
+
+        .panel-superior .derecho span {
+            float: right;
+        }
+
+        .panel-superior .derecho {
+            background: linear-gradient(to right, rgba(30, 87, 153, 0) 0%, rgba(30, 87, 153, 1) 53%, rgba(30, 87, 153, 1) 100%);
+        }
+
+        .estadisticas p {
+            height: 0px;
+            font-size: 25px;
+            margin-top: 5px;
+        }
+
+        .marcadores {
+            width: 100%;
+            position: relative;
+            height: 60px;
+            overflow: hidden;
+            box-shadow: 0px 0px 0px 2px #fff;
+        }
+
+        .marcadores .derecho {
+            background: rgba(30, 87, 153, 1);
+            box-shadow: 0px 0px 0px 3px #000 inset, 0px 0px 25px 0px #000 inset;
+            border-radius: 0px 10px 10px 0px;
+        }
+
+        .marcadores .izquierdo {
+            background: rgb(179, 121, 48);
+            box-shadow: 0px 0px 0px 3px #000 inset, 0px 0px 25px 0px #000 inset;
+            border-radius: 10px 0px 0px 10px;
+        }
+
+        .marcadores div {
+            float: left;
+            height: 100%;
+            line-height: 35px;
+            text-align: center;
+        }
+
+        .marcadores div p {
+            margin: 10px 0px 0px 5px;
+            width: 60%;
+            font-size: 24px;
+        }
+
+        .equipo-marcador {
+            width: 40%;
+        }
+
+        .marcador-tiempo {
+            width: 20%;
+            font-size: 26px;
+
+            line-height: 50px !important;
+        }
+
+        .marcador-tiempo time {
+            line-height: 50px;
+        }
+
+        .marcador-goles {
+            background: white;
+            width: 25%;
+            height: 40px !important;
+            margin: 10px 5px;
+            box-shadow: 0px 0px 0px 1px black;
+            color: #000;
+            line-height: 36px !important;
+            text-align: center;
+            font-size: 24px;
+            /*margin-left: 33px;*/
+        }
+
+        .equipo-inferor {
+            width: 20%;
+            /* line-height: 38px; */
+            /* vertical-align: middle; */
+            text-align: center;
+        }
+
+        .imagen-estrella {
+            width: 30px;
+            margin: 20px 0px 0px 1px;
+        }
+
+        .inferior-boton {
+
+            text-align: center;
+        }
+
+        .inferior-boton button {
+            background-color: #e8065b;
+            border: 2px solid #fff;
+            border-radius: 30px;
+            padding: 5px 15px;
+            box-shadow: 0px 0px 10px 2px black inset;
+            color: #fff;
+            font-weight: 600;
+            margin-top: 15px;
+            font-size: 20px;
+            transition: all 0.5s ease-out;
+        }
+
+        .inferior-boton button:hover {
+            background-color: #e8065b;
+            box-shadow: 0px 0px 3px 1px black inset;
+        }
+
+        .inferior-boton button:active {
+            background-color: #e8065b;
+            box-shadow: 0px 0px 0px 0px black inset;
+        }
+
+        .estadisticas {
+            text-align: center;
+        }
+
+        @media only screen and (min-device-width: 320px) and (max-device-width: 480px) {
+            .com{
+                font-size: 13px;
+                font-family: bebas, Verdana, Geneva, sans-serif;
+            }
+            
+            .base {
+                width: 90%;
+                margin: 0px auto;
+                box-shadow: 1px 1px 1px 1px black;
+                border-radius: 10px;
+                background: rgba(0, 0, 0, 1);
+                overflow: hidden;
+                border: 2px solid white;
+                font-family: bebas, Verdana, Geneva, sans-serif;
+                color: white;
+            }
+
+            .panel {
+                width: 100%;
+                box-shadow: 0px 0px 0px 1px rgba(255, 255, 255, 1);
+                background-color: rgba(0, 0, 0, 1);
+                /* display: block; */
+                /* float: left; */
+                position: relative;
+                top: 0px;
+                height: 50px;
+            }
+
+            .equipo-superior {
+                width: 50%;
+                float: left;
+                color: white;
+                height: 100%;
+                overflow: hidden;
+            }
+
+            .equipo-superior img {
+                //max-width: 13vw;
+                width: 60px;
+                margin: 0px 10px;
+            }
+
+            .float-izquierda {
+                float: left !important;
+            }
+
+            .float-derecha {
+                float: right !important;
+            }
+
+            .panel-superior .izquierdo {
+                background: linear-gradient(to right, rgba(255, 175, 75, 1) 0%, rgba(255, 146, 10, 0) 100%);
+            }
+
+            .panel-superior .izquierdo span {
+                float: left;
+            }
+
+            .panel-superior .derecho span {
+                float: right;
+            }
+
+            .panel-superior .derecho {
+                background: linear-gradient(to right, rgba(30, 87, 153, 0) 0%, rgba(30, 87, 153, 1) 53%, rgba(30, 87, 153, 1) 100%);
+            }
+
+            .estadisticas p {
+                height: 0px;
+                font-size: 12px;
+            }
+
+            .marcadores {
+                width: 100%;
+                position: relative;
+                height: 40px;
+                overflow: hidden;
+                box-shadow: 0px 0px 0px 2px #fff;
+            }
+
+            .marcadores .derecho {
+                background: rgba(30, 87, 153, 1);
+                box-shadow: 0px 0px 0px 3px #000 inset, 0px 0px 25px 0px #000 inset;
+                border-radius: 0px 10px 10px 0px;
+            }
+
+            .marcadores .izquierdo {
+                background: rgb(179, 121, 48);
+                box-shadow: 0px 0px 0px 3px #000 inset, 0px 0px 25px 0px #000 inset;
+                border-radius: 10px 0px 0px 10px;
+            }
+
+            .marcadores div {
+                float: left;
+                height: 100%;
+                line-height: 35px;
+                text-align: center;
+            }
+
+            .marcadores div p {
+                margin: 0px 0px 0px 5px;
+
+                width: 60%;
+                font-size: 10px;
+            }
+
+            .equipo-marcador {
+                width: 40%;
+            }
+
+            .marcador-tiempo {
+                width: 20%;
+                font-size: 16px;
+                line-height: 35px !important;
+            }
+
+            .marcador-tiempo time {
+                line-height: 35px !important;
+            }
+
+
+
+            .equipo-inferor {
+                width: 33%;
+                /* line-height: 38px; */
+                /* vertical-align: middle; */
+                text-align: center;
+            }
+
+
+
+            .imagen-estrella {
+                width: 16px;
+                margin: 15px 0px 0px 1px;
+            }
+
+            .inferior-boton {
+                text-align: center;
+            }
+
+            .inferior-boton button {
+                background-color: #e8065b;
+                border: 2px solid #fff;
+                border-radius: 30px;
+                padding: 2px 15px;
+                box-shadow: 0px 0px 10px 2px black inset;
+                color: #fff;
+                font-weight: 600;
+                margin-top: 10px;
+                transition: all 0.5s ease-out;
+                font-size: 16px;
+            }
+
+            .inferior-boton button:hover {
+                background-color: #e8065b;
+                box-shadow: 0px 0px 3px 1px black inset;
+            }
+
+            .inferior-boton button:active {
+                background-color: #e8065b;
+                box-shadow: 0px 0px 0px 0px black inset;
+            }
+
+            .estadisticas {
+                text-align: center;
+            }
+        }
+    </style>
+@endpush
