f43f551bea4027f680ae7d387391cff2e354296a
Hazrat02 <hazratbd80@gmail.com>
2024-01-17 08:14:43 -0800

    user bat details

--- core/resources/views/templates/basic/user/bet_log.blade.php
+++ core/resources/views/templates/basic/user/bet_log.blade.php
@@ -15,6 +15,7 @@
                                             <th>@lang('Result')</th>
                                             <th>@lang('Invest')</th>
                                             <th>@lang('Win or Lost')</th>
+                                            <th>@lang('Action')</th>
                                         </tr>
                                     </thead>
                                     <tbody>
@@ -55,6 +56,27 @@
                                                 @endif
                                                     @endif
                                                 </td>
+                                                @php
+                                                    $details = $log != null ? json_encode($log) : null;
+                                                @endphp
+
+                                                <td>
+                                                    <a class="btn
+                                                     base--bg btn-sm 
+                                                     @if ($details) 
+                                                     detailBtn @else disabled @endif" 
+                                                     href="javascript:void(0)"
+                                                     
+                                                     data-info="{{ $details }}"
+                                                      @endif
+                                                       {{-- @if ($deposit->status == Status::PAYMENT_REJECT)
+                                                      data-admin_feedback="{{ $log }}" 
+                                                      @endif --}}
+                                                      
+                                                      >
+                                                        <i class="fa fa-desktop"></i>
+                                                    </a>
+                                                </td>
                                             </tr>
                                         @empty
                                             <tr>
@@ -75,4 +97,63 @@
             </div>
         </div>
     </div>
+    <div class="modal fade" id="detailModal" role="dialog" tabindex="-1">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content section--bg">
+                <div class="modal-header">
+                    <h5 class="modal-title">@lang('Details')</h5>
+                    <span class="close" data-bs-dismiss="modal" type="button" aria-label="Close">
+                        <i class="las la-times"></i>
+                    </span>
+                </div>
+                <div class="modal-body">
+                    <ul class="list-group list-group-flush payment-list userData mb-2">
+                    </ul>
+                    <div class="feedback"></div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
+@push('script')
+    <script>
+        (function($) {
+            "use strict";
+            $('.detailBtn').on('click', function() {
+                var modal = $('#detailModal');
+
+                var userData = $(this).data('info');
+                var html = '';
+                if (userData) {
+                    userData.forEach(element => {
+                        if (element.type != 'file') {
+                            html += `
+                            <li class="list-group-item d-flex justify-content-between align-items-center">
+                                <span>Match:</span>
+                                <span">${element.value}</span>
+                            </li>`;
+                        }
+                    });
+                }
+
+                modal.find('.userData').html(html);
+
+                if ($(this).data('admin_feedback') != undefined) {
+                    var adminFeedback = `
+                        <div class="my-3">
+                            <strong>@lang('Admin Feedback')</strong>
+                            <p>${$(this).data('admin_feedback')}</p>
+                        </div>
+                    `;
+                } else {
+                    var adminFeedback = '';
+                }
+
+                modal.find('.feedback').html(adminFeedback);
+
+
+                modal.modal('show');
+            });
+        })(jQuery);
+    </script>
+@endpush
