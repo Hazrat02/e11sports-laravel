47b77f812f4336abba1c108b238bd502523716f1
Hazrat02 <hazratbd80@gmail.com>
2024-01-17 09:00:19 -0800

    user bat details

--- core/app/Http/Controllers/User/Auth/ForgotPasswordController.php
+++ core/app/Http/Controllers/User/Auth/ForgotPasswordController.php
@@ -38,40 +38,40 @@ class ForgotPasswordController extends Controller
 
     public function sendResetCodeEmail(Request $request)
     {
-        // $request->validate([
-        //     'value'=>'required'
-        // ]);
-        // $fieldType = $this->findFieldType();
-        // $user = User::where($fieldType, $request->value)->first();
+        $request->validate([
+            'value'=>'required'
+        ]);
+        $fieldType = $this->findFieldType();
+        $user = User::where($fieldType, $request->value)->first();
 
-        // if (!$user) {
-        //     $notify[] = ['error', 'Couldn\'t find any account with this information'];
-        //     return back()->withNotify($notify);
-        // }
+        if (!$user) {
+            $notify[] = ['error', 'Couldn\'t find any account with this information'];
+            return back()->withNotify($notify);
+        }
 
-        // PasswordReset::where('email', $user->email)->delete();
-        // $code = verificationCode(6);
-        // $password = new PasswordReset();
-        // $password->email = $user->email;
-        // $password->token = $code;
-        // $password->created_at = \Carbon\Carbon::now();
-        // $password->save();
+        PasswordReset::where('email', $user->email)->delete();
+        $code = verificationCode(6);
+        $password = new PasswordReset();
+        $password->email = $user->email;
+        $password->token = $code;
+        $password->created_at = \Carbon\Carbon::now();
+        $password->save();
 
-        // $userIpInfo = getIpInfo();
-        // $userBrowserInfo = osBrowser();
-        // notify($user, 'PASS_RESET_CODE', [
-        //     'code' => $code,
-        //     'operating_system' => @$userBrowserInfo['os_platform'],
-        //     'browser' => @$userBrowserInfo['browser'],
-        //     'ip' => @$userIpInfo['ip'],
-        //     'time' => @$userIpInfo['time']
-        // ],['email']);
+        $userIpInfo = getIpInfo();
+        $userBrowserInfo = osBrowser();
+        notify($user, 'PASS_RESET_CODE', [
+            'code' => $code,
+            'operating_system' => @$userBrowserInfo['os_platform'],
+            'browser' => @$userBrowserInfo['browser'],
+            'ip' => @$userIpInfo['ip'],
+            'time' => @$userIpInfo['time']
+        ],['email']);
 
-        // $email = $user->email;
-        // session()->put('pass_res_mail',$email);
-        // $notify[] = ['success', 'Password reset email sent successfully'];
-        // return to_route('user.password.code.verify')->withNotify($notify);
-        return 'working';
+        $email = $user->email;
+        session()->put('pass_res_mail',$email);
+        $notify[] = ['success', 'Password reset email sent successfully'];
+        return to_route('user.password.code.verify')->withNotify($notify);
+       
     }
 
     public function findFieldType()
