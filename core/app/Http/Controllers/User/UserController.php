84ba761ac344bfff28c3c682bfd975dfc6d567e7
Hazrat02 <hazratbd80@gmail.com>
2024-01-20 09:39:19 -0800

    match drow

--- core/app/Http/Controllers/User/UserController.php
+++ core/app/Http/Controllers/User/UserController.php
@@ -224,9 +224,10 @@ class UserController extends Controller {
     public function betLog() {
         $pageTitle = "Bet Game Logs";
         $play = bet_log::where('user_id', auth()->id())->latest();
+        $play2 = $play->get();
         $logs      = $play->with('betdata')->paginate(getPaginate());
-        $loss      = $play->where('winorloss', 'loss')->sum('amount');
-        $win      = $play->where('winorloss', 'win')->sum('winamount');
+        $loss      = $play2->where('winorloss', 'loss')->sum('amount');
+        $win      = $play2->where('winorloss', 'win')->sum('winamount');
         return view($this->activeTemplate . 'user.bet_log', compact('pageTitle', 'logs','win','loss'));
     }
 
